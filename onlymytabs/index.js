48b8d1664c419ff4424afbe4ac5a2b25a458d50e
comradeskolopendra <91865240+comradeskolopendra@users.noreply.github.com>
2021-11-30 12:02:04 +0600

    Add files via upload

--- onlymytabs/index.js
+++ onlymytabs/index.js
@@ -3,6 +3,11 @@ let outer = document.querySelector('.modal__outer');
 let inner = document.querySelector('.modal__inner');
 let nav = document.querySelectorAll('#tab')
 let contents = document.querySelectorAll('[data-content-active]');
+let steps = document.querySelectorAll('[data-active]');
+let next = document.querySelector('#next');
+let back = document.querySelector('#back');
+let card_show = document.querySelector('#show_card');
+let for_test_some_shit = contents.length;
 
 
 button_open.onclick = () => {
@@ -18,15 +23,88 @@ inner.onclick = (event) => {
     outer.classList.add('show');
 }
 
-contents.forEach((i) => {i.classList.add('display_none')});
+for (let i = 1; i < contents.length; i++) {
+    contents[i].classList.add('display_none')
+}
 
-nav.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
-        console.log(element);
-        contents.forEach((i) => {i.classList.add('display_none')});
-        const target = event.currentTarget;
-        const content = document.querySelector(`[data-content-active="${target.dataset.active}"]`);
-        content.classList.remove('display_none')
-    })
-})
+next.onclick = (event) => {
+    event.preventDefault();
+    const target = event.currentTarget;
+    const content = document.querySelector(`[data-content-active="${target.dataset.active}"]`)
+    content.classList.add('display_none')
+    content.nextElementSibling.classList.remove('display_none')
+    if (content.nextElementSibling.getAttribute('data-content-active') < contents.length) {
+        target.dataset.active = content.nextElementSibling.getAttribute('data-content-active');
+    }
+    else {
+        return false
+    }
+}
+
+back.onclick = (event) => {
+    event.preventDefault();
+    let target = 3;
+    const content = document.querySelector(`[data-content-active="${target}"]`)
+    content.classList.add('display_none')
+    content.previousElementSibling.classList.remove('display_none')
+    if (content.previousElementSibling.getAttribute('data-content-active') < contents.length) {
+        target = content.previousElementSibling.getAttribute('data-content-active');
+        console.log(content.previousElementSibling.getAttribute('data-content-active'))
+    }
+    else if (content.previousElementSibling.getAttribute('data-content-active') == 1) {
+        return false
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// nav.forEach((element) => {
+//     element.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         contents.forEach((i) => { i.classList.add('display_none') });
+//         const target = event.currentTarget;
+//         const content = document.querySelector(`[data-content-active="${target.dataset.active}"]`);
+//         content.classList.remove('display_none');
+//         next.onclick = () => {
+//             content.classList.add('display_none')
+//             content.nextElementSibling.classList.remove('display_none');
+//         }
+//         back.onclick = () => {
+//             content.classList.add('display_none')
+//             content.previousElementSibling.classList.remove('display_none')
+//         }
+//     })
+// })
