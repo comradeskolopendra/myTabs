cd7145309637e2a3cf52c2fbd491849c65ac8848
comradeskolopendra <91865240+comradeskolopendra@users.noreply.github.com>
2021-12-02 11:12:39 +0600

    Add files via upload

--- onlymytabs/index.js
+++ onlymytabs/index.js
@@ -27,47 +27,23 @@ for (let i = 1; i < contents.length; i++) {
     contents[i].classList.add('display_none')
 }
 
-// next.onclick = (event) => {
-//     event.preventDefault();
-//     const target = event.currentTarget;
-//     const content = document.querySelector(`[data-content-active="${target.dataset.active}"]`)
-//     content.classList.add('display_none')
-//     content.nextElementSibling.classList.remove('display_none')
-//     if (content.nextElementSibling.getAttribute('data-content-active') < contents.length) {
-//         target.dataset.active = content.nextElementSibling.getAttribute('data-content-active');
-//     }
-//     else if (content.nextElementSibling.getAttribute('data-content-active') == 3) {
-//         return false
-//     }
-// }
-
-// back.onclick = (event) => {
-//     event.preventDefault();
-//     const content = document.querySelector(`[data-content-active="${c}"]`)
-//     content.classList.add('display_none')
-//     content.previousElementSibling.classList.remove('display_none')
-//     c -= 1
-//     if (content.nextElementSibling.getAttribute('data-content-active' <= 1) && c <= 1) {
-//         return null;
-//     }
-// }
-
 next.addEventListener('click', (event) => {
     event.preventDefault();
 
-    if (counter <= contents.length) {
+    if (counter <= contents.length-1) {
         counter += 1
     }
     const content = document.querySelector(`[data-content-active="${counter}"]`)
     const previous_content = document.querySelector(`[data-content-active="${counter-1}"]`)
     content.classList.remove('display_none')
     previous_content.classList.add('display_none')
+    console.log(counter)
 })
 
 back.addEventListener('click', (event) => {
     event.preventDefault();
 
-    if (counter >= 1) {
+    if (counter >= 2) {
         counter -= 1
     }
     const content = document.querySelector(`[data-content-active="${counter}"]`)
@@ -75,55 +51,3 @@ back.addEventListener('click', (event) => {
     content.classList.remove('display_none')
     previous_content.classList.add('display_none')
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// nav.forEach((element) => {
-//     element.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         contents.forEach((i) => { i.classList.add('display_none') });
-//         const target = event.currentTarget;
-//         const content = document.querySelector(`[data-content-active="${target.dataset.active}"]`);
-//         content.classList.remove('display_none');
-//         next.onclick = () => {
-//             content.classList.add('display_none')
-//             content.nextElementSibling.classList.remove('display_none');
-//         }
-//         back.onclick = () => {
-//             content.classList.add('display_none')
-//             content.previousElementSibling.classList.remove('display_none')
-//         }
-//     })
-// })
