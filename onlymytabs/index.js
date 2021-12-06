289b3d20886aa9703bf1c6edd6c5b512bca94f97
comradeskolopendra <91865240+comradeskolopendra@users.noreply.github.com>
2021-12-06 11:31:08 +0600

    Add files via upload

--- onlymytabs/index.js
+++ onlymytabs/index.js
@@ -1,3 +1,4 @@
+// desktop
 let button_open = document.querySelector('#button__open');
 let outer = document.querySelector('.modal__outer');
 let inner = document.querySelector('.modal__inner');
@@ -7,7 +8,59 @@ let steps = document.querySelectorAll('[data-active]');
 let next = document.querySelector('#next');
 let back = document.querySelector('#back');
 let card_show = document.querySelector('#show_card');
+let modal__outerX2 = document.querySelectorAll('.modal__outerX2');
 let counter = 1;
+
+// mobile
+let button_open_mobile_modal = document.querySelector('#button__open_mobile');
+let container_mobile_modal = document.querySelector('.mobile__modal');
+let back_mobile_button = document.querySelector('#button__back_mob');
+let contents_mobile = document.querySelectorAll('[data-content-active-mobile]')
+let next_mobile = document.querySelector('#next_mobile');
+let back_mobile = document.querySelector('#back_mobile');
+let counter_mobile = 1;
+
+for (let k = 1; k < contents_mobile.length; k++) {
+    contents_mobile[k].classList.add('display_none')
+}
+
+next_mobile.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (counter_mobile <= contents_mobile.length-1) {
+        counter_mobile += 1;
+    }
+
+    const content_mobile = document.querySelector(`[data-content-active-mobile="${counter_mobile}"]`)
+    const previous_mobile = document.querySelector(`[data-content-active-mobile="${counter_mobile-1}"]`)
+    content_mobile.classList.remove('display_none')
+    previous_mobile.classList.add('display_none')
+})
+
+back_mobile.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (counter_mobile >= 2) {
+        counter_mobile -= 1
+    }
+
+    const content_mobile = document.querySelector(`[data-content-active-mobile="${counter_mobile}"]`)
+    const previous_mobile = document.querySelector(`[data-content-active-mobile="${counter_mobile+1}"]`)
+    content_mobile.classList.remove('display_none');
+    previous_mobile.classList.add('display_none')
+
+})
+
+
+button_open_mobile_modal.onclick = (event) => {
+    event.preventDefault();
+    container_mobile_modal.classList.toggle('display_none')
+}
+
+back_mobile_button.addEventListener('click', (event) => {
+    event.preventDefault();
+    container_mobile_modal.classList.toggle('display_none')
+})
 
 
 button_open.onclick = () => {
@@ -37,7 +90,6 @@ next.addEventListener('click', (event) => {
     const previous_content = document.querySelector(`[data-content-active="${counter-1}"]`)
     content.classList.remove('display_none')
     previous_content.classList.add('display_none')
-    console.log(counter)
 })
 
 back.addEventListener('click', (event) => {
